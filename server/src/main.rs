9ff0800cb079c122b4bc793749ea9736a172a094
Haf <haf@protonmail.ch>
2022-12-21 07:34:13 +0100

    default logging level to info

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, BufRead, Result};
 use log::info;
+use env_logger::Env;
 
 use bufstream::BufStream;
 
@@ -29,8 +30,11 @@ fn handle_client(conn: TcpStream) -> Result<()> {
 
 // TODO: Multithreaded
 // TODO: Port from clap
+// TODO: Log connection drop
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder
+              ::from_env(Env::default().default_filter_or("info"))
+              .init();
 
     let listener = TcpListener::bind("0.0.0.0:3333")?;
 
