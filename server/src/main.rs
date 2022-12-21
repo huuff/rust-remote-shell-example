f7a5a2019bac03f7fd2444c319220b6d42021311
Haf <haf@protonmail.ch>
2022-12-21 07:40:08 +0100

    no longer sending all previous messages

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,12 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, BufRead, Result};
-use log::info;
+use log::{info, trace};
 use env_logger::Env;
 
 use bufstream::BufStream;
 
 fn handle_client(conn: TcpStream) -> Result<()> {
+    let peer_addr = conn.peer_addr()?.to_string();
     let mut request = String::with_capacity(512);
     let mut stream = BufStream::new(&conn);
 
@@ -13,6 +14,9 @@ fn handle_client(conn: TcpStream) -> Result<()> {
         // Send prompt to client
         stream.write_all(b"> ")?;
         stream.flush()?;
+        trace!("Sent prompt to {}", peer_addr);
+
+        request.clear();
 
         let request_size = stream.read_line(&mut request)?;
         
@@ -23,16 +27,16 @@ fn handle_client(conn: TcpStream) -> Result<()> {
         // Just echo the request to the client
         stream.write_all(request.as_bytes())?;
         stream.flush()?;
+        trace!("Sent {} to {}", request, peer_addr);
     }
 
-    info!("Dropping connection from {}", conn.peer_addr()?.to_string());
+    info!("Dropping connection from {}", peer_addr);
 
     Ok(())
 }
 
 // TODO: Multithreaded
 // TODO: Port from clap
-// TODO: It's returning all previous messages. Fix it.
 fn main() -> Result<()> {
     env_logger::Builder
               ::from_env(Env::default().default_filter_or("info"))
