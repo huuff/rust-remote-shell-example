0fbb08de068cffcd1eabacfe9d615a9bfdeb2319
Haf <haf@protonmail.ch>
2022-12-24 11:41:58 +0100

    barely working

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,28 @@
 mod args;
+mod writeline;
 
 use crate::args::Args;
+use bufstream::BufStream;
 use clap::Parser;
+use std::{io::{Result, BufRead, self}, net::TcpStream};
+use writeline::WriteLine;
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
 
-    println!("Passed args: {:?}", args);
+    let addr = format!("{}:{}", args.host, args.port);
+    let conn = TcpStream::connect(addr)?;
+    let mut stream = BufStream::new(&conn);
+    let mut request = String::with_capacity(512);
+    let mut response = String::with_capacity(4096);
+
+    loop {
+        response.clear();
+        stream.read_line(&mut response)?;
+        println!("{}", response);
+        request.clear();
+        io::stdin().read_line(&mut request)?;
+        stream.write_line(request.as_str())?;
+    }
+
 }
